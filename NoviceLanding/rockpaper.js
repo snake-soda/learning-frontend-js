03c0381fc2b778462a551706a7eedcd64f19a845
Neythan Escapos <hello.escapos@gmail.com>
2023-01-16 05:51:46 -0600

    Beginning of RPS Game functionality

---
+++ NoviceLanding/rockpaper.js
@@ -0,0 +1,30 @@
+//First, let's set our defaults
+
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+//Now, we'll set conditions for either winning, losing, or ending in a tie.
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    }
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ) {
+        playerScore++
+        roundWinner = 'player'
+    }
+    if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ) {
+        computerScore++
+        roundWinner = 'computer'
+    }
+    updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
