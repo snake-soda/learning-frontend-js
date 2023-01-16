7a4ad78e96b91a14b94daab1c8868e956d146801
Neythan Escapos <hello.escapos@gmail.com>
2023-01-16 08:00:01 -0600

    Added: Random selector, gameover recognizer, const

--- NoviceLanding/rockpaper.js
+++ NoviceLanding/rockpaper.js
@@ -28,3 +28,40 @@ function playRound(playerSelection, computerSelection) {
     }
     updateScoreMessage(roundWinner, playerSelection, computerSelection)
 }
+
+//This will allow the computer to randomly select one of three options
+
+function getRandomChoice() {
+    let randomNumber = Math.floor(Math.random()*3)
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1:
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+    }
+}
+
+//When tally reaches 5 for either side, restart
+
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5
+}
+
+//Let's assign JavaScript constants to their corresponding DOM elements
+
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
+
